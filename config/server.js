ce3d601e900a36d520bf608543b3591d67b67b60
luisfalconmx <me@luisfalconmx.com>
2021-02-20 07:53:40 -0600

    feat: Add custom url for admin page #2

--- config/server.js
+++ config/server.js
@@ -3,6 +3,7 @@ module.exports = ({ env }) => ({
   port: env.int('PORT', 1337),
   url: env('URL'),
   admin: {
+    url: env('ADMIN_URL'),
     auth: {
       secret: env('ADMIN_JWT_SECRET'),
     },
