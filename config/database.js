0fadce017f8168a363c8ddf7dba0288dfa5f83c7
luisfalconmx <me@luisfalconmx.com>
2021-06-05 02:14:58 -0500

    feat: added support for using a MySQL database

--- config/database.js
+++ config/database.js
@@ -2,19 +2,17 @@ module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
-      connector: 'mongoose',
+      connector: 'bookshelf',
       settings: {
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        srv: env.bool('DATABASE_SRV', false),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'strapi'),
-        username: env('DATABASE_USERNAME', 'user'),
-        password: env('DATABASE_PASSWORD', 'password'),
+        client: 'mysql',
+        host: env('DATABASE_HOST'),
+        port: env.int('DATABASE_PORT'),
+        database: env('DATABASE_NAME'),
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
+        ssl: env.bool('DATABASE_SSL'),
       },
-      options: {
-        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-        ssl: env.bool('DATABASE_SSL', false),
-      },
+      options: {},
     },
   },
 })
