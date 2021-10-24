68579fc447aaa3a0e685eff66fe80d7a57c40e39
luisfalconmx <luisfalconmx@gmail.com>
2021-10-24 03:41:37 -0500

    docs: readme file improved, eslint parser updated and default values set in database config

--- config/database.js
+++ config/database.js
@@ -6,11 +6,11 @@ module.exports = ({ env }) => ({
       settings: {
         client: 'mysql',
         host: env('DATABASE_HOST'),
-        port: env.int('DATABASE_PORT'),
-        database: env('DATABASE_NAME'),
+        port: env.int('DATABASE_PORT', 3306),
+        database: env('DATABASE_NAME', 'strapi'),
         username: env('DATABASE_USERNAME'),
         password: env('DATABASE_PASSWORD'),
-        ssl: env.bool('DATABASE_SSL')
+        ssl: env.bool('DATABASE_SSL', false)
       },
       options: {}
     }
